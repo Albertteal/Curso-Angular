89f2f7b1cbf55f6de8322aea914f27b31e6c1c8b
Albert Eduardo Núñez Nieto <albertnunez@pumpingteam.com>
2024-03-06 09:41:17 -0600

    Se agregan los estilos correspondientes a el home y el html correspondiente al mismo

--- proyecto-angular/src/app/app-routing.module.ts
+++ proyecto-angular/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
+import { last } from 'rxjs';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'labs',
+    component: LabsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
